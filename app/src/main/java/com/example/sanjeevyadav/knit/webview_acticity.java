674d6051ab6188a9a1d16ef8582b4c86ccfdc449
Ashish Kumar <ashishkmrydv392@gmail.com>
2019-02-04 01:59:21 +0530

    WebView activity completed

---
+++ app/src/main/java/com/example/sanjeevyadav/knit/webview_acticity.java
@@ -0,0 +1,96 @@
+package com.example.sanjeevyadav.knit;
+
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+import android.support.v7.widget.Toolbar;
+import android.view.View;
+import android.webkit.WebChromeClient;
+import android.webkit.WebSettings;
+import android.webkit.WebView;
+import android.webkit.WebViewClient;
+import android.widget.ProgressBar;
+import android.graphics.Bitmap;
+
+import java.net.MalformedURLException;
+import java.net.URL;
+
+public class webview_acticity extends AppCompatActivity {
+
+    private WebView webView;
+    private ProgressBar progressBar;
+    private Toolbar toolbar;
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_webview_acticity);
+
+        toolbar= findViewById(R.id.toolbar_id);
+        setSupportActionBar(toolbar);
+        webView = findViewById(R.id.webview_id);
+        progressBar = findViewById(R.id.progressbar_id);
+        myWebClient webclient = new myWebClient();
+        webView.setWebViewClient(webclient);
+        webView.setWebChromeClient(new WebChromeClient() {
+            @Override
+            public void onProgressChanged(WebView view, int newProgress) {
+                progressBar.setProgress(newProgress);
+                if(newProgress==100){
+                    progressBar.setVisibility(View.GONE);
+                }else{
+                    progressBar.setVisibility(View.VISIBLE);
+                }
+            }
+        });
+        WebSettings webSettings = webView.getSettings();
+        webSettings.setJavaScriptEnabled(true);
+        webView.setScrollBarStyle(View.SCROLLBARS_INSIDE_OVERLAY);
+        webView.getSettings().setLoadsImagesAutomatically(true);
+        webView.getSettings().setSupportZoom(true);
+        webView.getSettings().setBuiltInZoomControls(true);
+        webView.loadUrl("http://knit.ac.in/result_1.htm");
+        URL hostUrl= null;
+        try {
+            hostUrl = new URL(webView.getUrl());
+        } catch (MalformedURLException e) {
+            e.printStackTrace();
+        }
+        webclient.onPageStarted(webView,webView.getUrl(),null);
+        webclient.onPageFinished(webView,webView.getUrl());
+        webView.getSettings().setUseWideViewPort(true);
+        webView.getSettings().setLoadWithOverviewMode(true);
+    }
+
+    @Override
+    public void onBackPressed() {
+        if (webView.canGoBack()) {
+            webView.goBack();
+            return;
+        }
+        // Otherwise defer to system default behavior.
+        super.onBackPressed();
+    }
+
+    public class myWebClient extends WebViewClient
+    {
+        @Override
+        public void onPageStarted(WebView view, String url, Bitmap favicon) {
+            // TODO Auto-generated method stub
+            super.onPageStarted(view, url, favicon);
+            getSupportActionBar().setTitle(view.getTitle());
+        }
+
+        @Override
+        public boolean shouldOverrideUrlLoading(WebView view, String url) {
+            // TODO Auto-generated method stub
+            view.loadUrl(url);
+            return true;
+        }
+
+        @Override
+        public void onPageFinished(WebView view, String url) {
+            super.onPageFinished(view, url);
+            getSupportActionBar().setTitle(view.getTitle());
+        }
+    }
+}
