be6a2d9a440804700d428de544f164767bc921ad
Sanjeev <knit16643@gmail.com>
2019-02-03 22:31:09 +0530

    Added Academics and Admin.

---
+++ app/src/main/java/com/example/sanjeevyadav/knit/AdminstrationActivity.java
@@ -0,0 +1,13 @@
+package com.example.sanjeevyadav.knit;
+
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+
+public class AdminstrationActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_adminstration);
+    }
+}
