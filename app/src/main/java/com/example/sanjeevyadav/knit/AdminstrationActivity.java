088e1e19d430346bd8fd1c6bda3b91e68a4f051c
sanjeevsky <knit16643@gmail.com>
2019-02-21 11:13:12 +0530

    Added Pdf Links in Admin

--- app/src/main/java/com/example/sanjeevyadav/knit/AdminstrationActivity.java
+++ app/src/main/java/com/example/sanjeevyadav/knit/AdminstrationActivity.java
@@ -1,11 +1,15 @@
 package com.example.sanjeevyadav.knit;
 
+import android.content.Intent;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
 import android.support.v7.widget.Toolbar;
+import android.view.View;
+import android.widget.TextView;
 
 public class AdminstrationActivity extends AppCompatActivity {
     private Toolbar toolbar;
+    private TextView ByLaws,BoardsOfGoverners,MinutesBog,MembersofAcademic,ListOfficeBearers;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -16,5 +20,55 @@ public class AdminstrationActivity extends AppCompatActivity {
         getSupportActionBar().setTitle("Administration");
         getSupportActionBar().setDisplayHomeAsUpEnabled(true);
         getSupportActionBar().setDisplayShowHomeEnabled(true);
+        ByLaws=findViewById(R.id.pdf_laws);
+        BoardsOfGoverners=findViewById(R.id.pdf_boards_of_governers);
+        MinutesBog=findViewById(R.id.pdf_minutes_bog);
+        MembersofAcademic=findViewById(R.id.pdf_member_of_academics);
+        ListOfficeBearers=findViewById(R.id.pdf_list_of_office_bearers);
+
+        ByLaws.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                Intent adminIntent = new Intent(AdminstrationActivity.this, webview_acticity.class);
+                adminIntent.putExtra("pdf", "http://www.knit.ac.in/pdf/bylaws_mar0216.pdf");
+                startActivity(adminIntent);
+            }
+        });
+        BoardsOfGoverners.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                Intent adminIntent = new Intent(AdminstrationActivity.this, webview_acticity.class);
+                adminIntent.putExtra("pdf", "http://www.knit.ac.in/pdf/BOG%20Member.pdf");
+                startActivity(adminIntent);
+
+            }
+        });
+        MinutesBog.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                Intent adminIntent = new Intent(AdminstrationActivity.this, webview_acticity.class);
+                adminIntent.putExtra("pdf", "http://www.knit.ac.in/pdf/bog3oct_070118.pdf");
+                startActivity(adminIntent);
+
+            }
+        });
+        MembersofAcademic.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                Intent adminIntent = new Intent(AdminstrationActivity.this, webview_acticity.class);
+                adminIntent.putExtra("pdf", "http://www.knit.ac.in/pdf/AC2017.pdf");
+                startActivity(adminIntent);
+
+            }
+        });
+        ListOfficeBearers.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                Intent adminIntent = new Intent(AdminstrationActivity.this, webview_acticity.class);
+                adminIntent.putExtra("pdf", "http://www.knit.ac.in/pdf/beares_090818.pdf");
+                startActivity(adminIntent);
+
+            }
+        });
     }
 }
